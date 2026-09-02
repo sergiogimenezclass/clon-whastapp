4ffa32ca996740b7fad72c02be877445ce9a9fba
Sergio <sergio@example.com>
2026-09-02 10:38:37 -0300

    Maquetar la interfaz de WhatsApp Web

---
+++ app.js
@@ -0,0 +1,3 @@
+// En este archivo van a escribir la funcionalidad con JavaScript.
+// Sigan los prompts del README de a uno: cada prompt agrega una parte nueva.
+// Regla de oro: no modifican el HTML ni el CSS existente.
